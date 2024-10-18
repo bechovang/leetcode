70fc6fa5e8c44a405f6d9297b02ef07c6acb4525
bechovang <phuchcm2006@gmail.com>
2024-10-19 00:30:23 +0700

    19/10/2024

---
+++ 19-10-2024__Sliding Windows__424. Longest Repeating Character Replacement/main.cpp
@@ -0,0 +1,38 @@
+///Be cho vang
+
+#include <bits/stdc++.h>
+#define endl '\n'
+#define faster ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//#define int long long
+using namespace std;
+
+
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        
+    }
+};
+
+
+signed main()
+{
+    faster; 
+    freopen("input.inp","r",stdin);
+    freopen("output.out","w",stdout);
+    string str;
+    int k;
+
+    cin>>str;
+    cin>>k;
+    
+    
+
+    Solution s;
+    int ans= s.characterReplacement(str,k);
+    cout<<ans;
+
+    return 0;
+}
+
