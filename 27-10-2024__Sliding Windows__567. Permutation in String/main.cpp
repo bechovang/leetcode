975a4e31bd198a6f08081a913842e5a82cf5257e
bechovang <phuchcm2006@gmail.com>
2024-10-27 20:05:26 +0700

    27/10/2024

--- 27-10-2024__Sliding Windows__567. Permutation in String/main.cpp
+++ 27-10-2024__Sliding Windows__567. Permutation in String/main.cpp
@@ -1,5 +1,7 @@
 ///Be cho vang
 
+// permutation : hoán vị, vẫn các chữ đó nhưng đổi vị trí
+
 #include <bits/stdc++.h>
 #define endl '\n'
 #define faster ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -8,12 +10,35 @@ using namespace std;
 
 class Solution {
 public:
-    int romanToInt(string s) {
-        vector <int> a;
-        for (int i=0;i<s.size();i++){
-            
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) return false; // if s2 contains
+
+        map<char,int> s1Map;
+        for (int i=0;i<s1.size();i++){
+            s1Map[s1[i]]++;
         }
-        return 0;
+
+        
+        for (int i=0;i<s2.size()-s1.size()+1;i++){
+            map<char,int> s2Map;
+            for (int j=i;j<i+s1.size();j++){
+                s2Map[s2[j]]++;
+            }
+            bool check=true;
+            for (auto x: s2Map){
+                char k=x.first;
+                int i=x.second;
+                if (s1Map[k] != i){
+                    check=false;
+                    break;
+                }
+            }
+
+            if (check==true) return true;
+
+        }
+        return false;
+    
     }
 };
 
@@ -23,11 +48,11 @@ signed main()
     faster;
     freopen("input.inp","r",stdin);
     freopen("output.out","w",stdout);
-    string str;
-    cin>>str;
+    string str1, str2;
+    cin>>str1>>str2;
 
     Solution s;
-    int ans= s.romanToInt(str);
+    bool ans= s.checkInclusion(str1,str2);
     cout<<ans;
 
     return 0;
